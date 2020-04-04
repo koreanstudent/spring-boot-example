a7ea52be8d20b60ced9bd15268a5fd2899d0ff0c
koreanstudent <hn12344@naver.com>
2020-04-04 23:11:32 +0900

    인메모리 데이터 베이스

---
+++ springdata/src/main/java/com/example/springdata/H2Runner.java
@@ -0,0 +1,42 @@
+package com.example.springdata;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class H2Runner implements ApplicationRunner {
+	
+	@Autowired
+	DataSource dataSource;
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+
+		try(Connection connection = dataSource.getConnection()) {
+			
+			System.out.println(connection.getMetaData().getURL());
+			System.out.println(connection.getMetaData().getUserName());
+			
+			Statement statement = connection.createStatement();
+			String sql = "CREATE TABLE USER(ID INTEGER NOT NULL, NAME VARCHAR(255), PRIMARY KEY (id))";
+			statement.executeUpdate(sql);
+			
+//			connection.close();
+		}
+		
+		jdbcTemplate.execute("INSERT INTO USER VALUES (1,'chang')");
+	}
+	
+
+}
